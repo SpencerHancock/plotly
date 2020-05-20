8830234b0005fb74d07aaacb689164d7f67cdb3b
spencer <spencerjhancock@gmail.com>
2020-05-20 13:23:06 -0700

    ryan suggestions

--- app.py
+++ app.py
@@ -35,39 +35,71 @@ df["SP"], df["SPtemp"], df["SPNM"] = RV.SP(df["C_f"], df["C_p"], df["Q_f"], df["
 df["DPNM"] = RV.DP(df.Q_p, df.Q_f, df.T_f)
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-app.layout = html.Div(style={"margin":"auto", "height":"250vh", "text-align":"center"},children=[
-    html.Div([
-    dcc.Graph(style={"width":"70vw", "height":"30%","padding":"0px","margin":0},
-                id="graph-with-slider1"),
-    dcc.Graph(style={"width":"70vw", "height":"30%","padding":0,"margin":0},id="graph-with-slider2"),
-    dcc.Graph(style={"width":"70vw", "height":"30%","padding":0,"margin":0},id="graph-with-slider3"),
-    dcc.Slider(
-        id="day-slider",
-        min=min(df.index),
-        max=len(df.Datetime),
-        value=2
-            ),
-    html.P(id="html-date"),
-    html.P("These Graphs are horizontal bar graphs set to be 'relative', that means that the QNM and Qpress along with all the other small section are how much you would gain or lose given that change. They Do Not sum to an overall ideal value.")
-    ], style={"width":"70vw", "height":"100vh","padding":"0px","margin":"auto"}),
-    html.Div([
-    dcc.Graph(style={"width":"70vw", "height":"90%","padding":"0px","margin":0},
-                id="graph-with-slider4"),
-    dcc.Slider(
+app.layout = html.Div(style={"margin":"auto", "text-align":"center"},children=[
+    #==================================== Bullet Graph ============================================
+    html.Div(style={"height":"100vh"}, children=[
+    html.Div(style = {"height":"80%"}, className="row", children=[
+        html.Div(style={"height":"90%"}, className="col-sm-1"),
+        dcc.Graph(style={"height":"90%","padding":"0px","margin":0},
+                    id="graph-with-slider4", className="col-10"),
+        html.Div(style={"height":"90%"}, className="col-sm-1")
+    ]),
+    html.Div(style = {"font-size":"15px"},className="row", children=[
+        html.B("Select Date: ", className="col-2"),
+        dcc.Slider(
         id="day-slider2",
         min=min(df.index),
         max=len(df.Datetime),
-        value=2
+        value=2,
+        className="col-8"
             ),
-    html.P(id="html-date2"),
-    html.P("The Bullet graph listed below represent Qrefpress in grey, QNM in green and Qp in blue. The right side indicates loss from QNM")
-    ], style={"width":"70vw", "height":"100vh","padding":"60px","margin":"auto"})
- ] )
+        html.B(id="html-date2", className="col-2")
+
+    ]),
+
+    html.P("""The Bullet graph listed above represents real time changes in pressure drop, 
+            salt passage and product flow rate. On the Flow Rate Graph the grey indicates 
+            the current flow rate at reference pressure, the blue is current product flow rate, 
+            and the red line is what the flow rate could be given a new membrane. For Salt Passage which is 
+            affected differently by conditions and fouling, the blue once again represents real-time Salt Passage, 
+            the grey is Salt Passage as reference temp but otherwise current conditions and the red line is salt 
+            passage with a new membrane at the current conditions. For Differential Pressure or Pressure Drop, the blue is real-
+            time and again the red line is pressure drop with a new membrane at the current conditions.""", style = {"font-size":"15px", "margin":"auto"}, className="col-10"),]),
+    #======================================= Bar Graph =============================================
+
+    html.Div(className="row", style={"height":"100vh"}, children=[
+
+        html.Div(style={"height":"90%"}, className="col-sm-1"),
+
+        html.Div([
+        dcc.Graph(style={"height":"30%"},id="graph-with-slider1"),
+        dcc.Graph(style={"height":"30%"},id="graph-with-slider2"),
+        dcc.Graph(style={"height":"30%"},id="graph-with-slider3")
+        ], className="col-10", style={"height":"100%"}),
+
+        html.Div(style={"height":"90%"}, className="col-sm-1"),
+    ]),
+    html.Div(style = {"font-size":"15px"},className="row", children=[
+        html.B("Select Date: ", className="col-2"),
+        dcc.Slider(
+        id="day-slider",
+        min=min(df.index),
+        max=len(df.Datetime),
+        value=2,
+        className="col-8"
+            ),
+        html.B(id="html-date", className="col-2")
+
+    ]),
+    html.P("""The bar graphs show all of the same information just differently. These Graphs are horizontal bar graphs set to be 'relative', 
+    that means that the changes due to reference pressure or a new membrane are the additive portion. 
+    Each smaller section indicates how much you would gain or lose given that change. They Do Not sum to an overall ideal value.""", style = {"font-size":"15px", "margin":"auto"}, className="col-10")
+    ])
 
 # def MakeBarPlot(X,Y, names):
 #     data = []
@@ -113,100 +145,111 @@ app.layout = html.Div(style={"margin":"auto", "height":"250vh", "text-align":"ce
     #             showticklabels=False
     #             ))}
 
-try:
-    @app.callback(
-    [Output('graph-with-slider1', 'figure'),
-    Output('graph-with-slider2', 'figure'),
-    Output('graph-with-slider3', 'figure'),
-     Output('html-date', 'children')],
-    [Input('day-slider', 'value')])
-    def update_barplot(selected_day):
-        selected_day = int(selected_day)
-        # filtered_df = df[df.datetime = selected_day]
-        return  [{"data":[{"x":[df.at[selected_day,"Q_p"]], "y":["Flow"], "type":"bar", "orientation":"h","name":"Flow"},
-                {"x":[df.at[selected_day,"QNM"]-df.at[selected_day,"Q_p"]], "y":["Flow"], "type":"bar", "orientation":"h","name":"New Membrane"},
-                {"x":[df.at[selected_day,"Qpress"]-df.at[selected_day,"Q_p"]], "y":["Flow"], "type":"bar", "orientation":"h","name":"Reference Pressure"}],
-                "layout": go.Layout(barmode="relative",title="Flow/Time",
-                yaxis=dict(
-                autorange=True,
-                showgrid=False,
-                ticks='',
-                showticklabels=False
-                ))},
-                {"data":[{"x":[df.at[selected_day,"SP"]], "y":["SP"], "type":"bar", "orientation":"h","name":"SP"},
-                {"x":[df.at[selected_day,"SPNM"]-df.at[selected_day,"SP"]], "y":["SP"], "type":"bar", "orientation":"h","name":"New Membrane"},
-                {"x":[df.at[selected_day,"SPtemp"]-df.at[selected_day,"SP"]], "y":["SP"], "type":"bar", "orientation":"h","name":"Reference Temp"}],
-                "layout": go.Layout(barmode="relative",title="Salt Passage/Time",
-                yaxis=dict(
-                autorange=True,
-                showgrid=False,
-                ticks='',
-                showticklabels=False
-                ))},
-                {"data":[{"x":[df.at[selected_day,"dp"]], "y":["DP"], "type":"bar", "orientation":"h","name":"DP"},
-                {"x":[df.at[selected_day,"DPNM"]-df.at[selected_day,"dp"]], "y":["DP"], "type":"bar", "orientation":"h","name":"New Membrane"}],
-                "layout": go.Layout(barmode="relative", title="Pressure Drop/Time",
-                yaxis=dict(
-                autorange=True,
-                showgrid=False,
-                ticks='',
-                showticklabels=False
-                ))},
-                df.at[selected_day, "Datetime"]]
-except: pass
-try:
-    @app.callback(
-        [Output('graph-with-slider4', 'figure'),
-        Output('html-date2', 'children')],
-        [Input('day-slider2', 'value')])
-    def update_bulletplot(selected_day):
-        fig = go.Figure()
-        fig.add_trace(go.Indicator(
-        mode = "number+gauge+delta", value = df.at[selected_day, "Q_p"],
-        delta = {'reference': df.at[selected_day, "QNM"]},
-        domain = {'x': [0.25, 1], 'y': [.05, .25]},
-        title = {'text': "Flow"},
-        gauge = {
-            'shape': "bullet",
-            'axis': {'range': [None, max(df.QNM)]},
-            'threshold': {
-                'line': {'color': "green", 'width': 2},
-                'thickness': 0.75,
-                'value': df.at[selected_day, "QNM"]},
-            'steps': [
-                {'range': [0, df.at[selected_day, "Qpress"]], 'color': "gray"},
-                {'range': [df.at[selected_day, "Qpress"], max(df.QNM)], 'color': "white"}],
-            'bar': {'color': "blue"}}))
-        fig.add_trace(go.Indicator(
-                    mode = "number+gauge+delta", value = df.at[selected_day, "SP"],
-                    delta = {'reference': df.at[selected_day, "SP"]},
-                    domain = {'x': [0.25, 1], 'y': [0.4, .6]},
-                    title = {'text': "SP"},
-                    gauge = {
-                        'shape': "bullet",
-                        'axis': {'range': [None, 2]},
-                        'threshold': {
-                            'line': {'color': "green", 'width': 2},
-                            'thickness': 0.75,
-                            'value': df.at[selected_day, "SPNM"]},
-                        'steps': [
-                            {'range': [0, df.at[selected_day, "SPtemp"]], 'color': "gray"},
-                            {'range': [df.at[selected_day, "SPtemp"], max(df.SPNM)], 'color': "white"}],
-                        'bar': {'color': "blue"}}))
-        fig.add_trace(go.Indicator(
-        mode = "number+gauge+delta", value = df.at[selected_day, "dp"],
-        delta = {'reference': df.at[selected_day, "DPNM"]},
-        domain = {'x': [0.25, 1], 'y': [.7,.9 ]},
-        title = {'text': "DP"},
-        gauge = {
-            'shape': "bullet",
-            'axis': {'range': [None, max(df.dp)]},
-            'threshold': {
-                'line': {'color': "green", 'width': 2},
-                'thickness': 0.75,
-                'value': df.at[selected_day, "DPNM"]},
-            'bar': {'color': "blue"}}))
-        return fig, df.at[selected_day, "Datetime"]
+
+@app.callback(
+[Output('graph-with-slider1', 'figure'),
+Output('graph-with-slider2', 'figure'),
+Output('graph-with-slider3', 'figure'),
+    Output('html-date', 'children')],
+[Input('day-slider', 'value')])
+def update_barplot(selected_day):
+    selected_day = int(selected_day)
+    # filtered_df = df[df.datetime = selected_day]
+    return  [{"data":[{"x":[df.at[selected_day,"Q_p"]], "y":["Flow"], "type":"bar", "orientation":"h","name":"Flow"},
+            {"x":[df.at[selected_day,"QNM"]-df.at[selected_day,"Q_p"]], "y":["Flow"], "type":"bar", "orientation":"h","name":"New Membrane"},
+            {"x":[df.at[selected_day,"Qpress"]-df.at[selected_day,"Q_p"]], "y":["Flow"], "type":"bar", "orientation":"h","name":"Reference Pressure"}],
+            "layout": go.Layout(barmode="relative",title="Flow/Time",
+            yaxis=dict(
+            autorange=True,
+            showgrid=False,
+            ticks='',
+            showticklabels=False
+            ))},
+            {"data":[{"x":[df.at[selected_day,"SP"]], "y":["SP"], "type":"bar", "orientation":"h","name":"SP"},
+            {"x":[df.at[selected_day,"SPNM"]-df.at[selected_day,"SP"]], "y":["SP"], "type":"bar", "orientation":"h","name":"New Membrane"},
+            {"x":[df.at[selected_day,"SPtemp"]-df.at[selected_day,"SP"]], "y":["SP"], "type":"bar", "orientation":"h","name":"Reference Temp"}],
+            "layout": go.Layout(barmode="relative",title="Salt Passage/Time",
+            yaxis=dict(
+            autorange=True,
+            showgrid=False,
+            ticks='',
+            showticklabels=False
+            ))},
+            {"data":[{"x":[df.at[selected_day,"dp"]], "y":["DP"], "type":"bar", "orientation":"h","name":"DP"},
+            {"x":[df.at[selected_day,"DPNM"]-df.at[selected_day,"dp"]], "y":["DP"], "type":"bar", "orientation":"h","name":"New Membrane"}],
+            "layout": go.Layout(barmode="relative", title="Pressure Drop/Time",
+            yaxis=dict(
+            autorange=True,
+            showgrid=False,
+            ticks='',
+            showticklabels=False
+            ))},
+            df.at[selected_day, "Datetime"]]
+@app.callback(
+    [Output('graph-with-slider4', 'figure'),
+    Output('html-date2', 'children')],
+    [Input('day-slider2', 'value')])
+def update_bulletplot(selected_day):
+    fig = go.Figure()
+    fig.add_trace(go.Indicator(
+    mode = "number+gauge+delta", value = df.at[selected_day, "Q_p"],
+    delta = {'reference': df.at[selected_day, "QNM"]},
+    domain = {'x': [0, 1], 'y': [.05, .25]},
+    title = {'text': "Flow"},
+    gauge = {
+        'shape': "bullet",
+        'axis': {'range': [None, max(df.QNM)]},
+        'threshold': {
+            'line': {'color': "green", 'width': 2},
+            'thickness': 0.75,
+            'value': df.at[selected_day, "QNM"]},
+        'steps': [
+            {'range': [0, df.at[selected_day, "Qpress"]], 'color': "gray"},
+            {'range': [df.at[selected_day, "Qpress"], max(df.QNM)], 'color': "white"}],
+        'bar': {'color': "blue"}}))
+    fig.add_trace(go.Indicator(
+                mode = "number+gauge+delta", value = df.at[selected_day, "SP"],
+                delta = {'reference': df.at[selected_day, "SPNM"]},
+                domain = {'x': [0, 1], 'y': [0.4, .6]},
+                title = {'text': "SP"},
+                gauge = {
+                    'shape': "bullet",
+                    'axis': {'range': [None, 2]},
+                    'threshold': {
+                        'line': {'color': "red", 'width': 2},
+                        'thickness': 0.75,
+                        'value': df.at[selected_day, "SPNM"]},
+                    'steps': [
+                        {'range': [0, df.at[selected_day, "SPtemp"]], 'color': "gray"},
+                        {'range': [df.at[selected_day, "SPtemp"], max(df.SPNM)], 'color': "white"}],
+                    'bar': {'color': "blue"}}))
+    fig.add_trace(go.Indicator(
+    mode = "number+gauge+delta", value = df.at[selected_day, "dp"],
+    delta = {'reference': df.at[selected_day, "DPNM"]},
+    domain = {'x': [0, 1], 'y': [.7,.9 ]},
+    title = {'text': "DP"},
+    gauge = {
+        'shape': "bullet",
+        'axis': {'range': [None, max(df.dp)]},
+        'threshold': {
+            'line': {'color': "red", 'width': 2},
+            'thickness': 0.75,
+            'value': df.at[selected_day, "DPNM"]},
+        'bar': {'color': "blue"}}))
+    
+    fig.update_layout(
+        title={
+        'text': "Normalized Performance Breakdown",
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+        xaxis_title="Time",
+        font=dict(
+            family="Courier New, monospace",
+            size=18,
+            color="#7f7f7f"))
+    return fig, df.at[selected_day, "Datetime"]
 
 
 
@@ -252,9 +295,6 @@ try:
 #             transition = {'duration': 500},
 #         )
 #     }
-
-except:
-    pass
 
 if __name__ == '__main__':
     app.run_server(debug=True)
